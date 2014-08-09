24950dd8d88b2a415008e945b9428594b3254cad
Julitogtu <julito_gtu@hotmail.com>
2014-08-09 16:32:34 -0500

    Demo finished

---
+++ DemoMvcMongoLab/DemoMvcMongoLab/Context/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace DemoMvcMongoLab.Context
+{
+    public class AppContext
+    {
+        public MongoDatabase Database;
+        readonly string connnectionstring = @"mongodb://DemoMvcMongoLab:z25DOUq0grml4Wy6uLwN3Wlg.ZZWdyGXkxIc0A5Hh0s-@ds045077.mongolab.com:45077/DemoMvcMongoLab";
+        readonly string databaseName = "DemoMvcMongoLab";
+
+        public AppContext()
+        {
+            var mongoClient = new MongoClient(connnectionstring);
+            var server = mongoClient.GetServer();
+            Database = server.GetDatabase(databaseName);
+        }
+    }
+}
---
+++ DemoMvcMongoLab/DemoMvcMongoLab/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DemoMvcMongoLab.Models
+{
+    public class Customer
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
